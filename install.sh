9cdae23418c96c7f1e0eb1aceebea52e6aefc3cf
ibnu7 <ibnudahlan7@gmail.com>
2024-01-18 20:46:58 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update dan upgrade Termux
+apt update
+apt upgrade -y
+
+# Instal Python
+pkg install python -y
+
+# Instal Node.js (LTS)
+pkg install nodejs-lts -y
+
+# Instal modul Python menggunakan pip
+pip install pycryptodome
+pip install cffi
+pip install xxtea-py
+pip install xxtea
+pip install tqdm
+pip install requests
+pip install colorama
